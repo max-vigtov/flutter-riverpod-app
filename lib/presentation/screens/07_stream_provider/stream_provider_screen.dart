00382777627b4178f613c91659eebd237f2f1919
Max Vigueras <mvt.2000@hotmail.com>
2025-03-04 03:41:39 -0600

    Riverpod: Stream Provided added.

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/stream_provider.dart';
 
 
 class StreamProviderScreen extends StatelessWidget {
@@ -10,6 +12,30 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, ref) {
+    final usersInChat = ref.watch( userInChatProvider );
+
+    if ( !usersInChat.hasValue ) {
+      return const Center(child: CircularProgressIndicator());      
+    }
+
+    return ListView.builder(
+      itemCount: usersInChat.value!.length,
+      itemBuilder: (context, index) {
+        final name = usersInChat.value![index];
+        return ListTile(
+          title: Text( name),
+        );
+      }
     );
   }
 }
